d7f01016307ac1e152d8b43225dfe20444e9c74c
mahmoudrabbas <mahmoudrabass11@gmail.com>
2025-04-13 08:37:36 +0200

    test scheduling

--- src/main/java/com/example/store/bookshop/BookshopApplication.java
+++ src/main/java/com/example/store/bookshop/BookshopApplication.java
@@ -2,6 +2,9 @@ package com.example.store.bookshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class BookshopApplication {
---
+++ src/main/java/com/example/store/bookshop/service/scheduling/Testy.java
@@ -0,0 +1,22 @@
+package com.example.store.bookshop.service.scheduling;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class Testy {
+    Logger logger = LoggerFactory.getLogger(Testy.class);
+
+    @Scheduled(fixedRate = 2000)
+    @Async
+    public void printEvery5Secs() throws InterruptedException {
+        Thread.sleep(6000);
+        logger.info("I am supposed to be print every 5 seconds {}", LocalDateTime.now());
+    }
+
+}
